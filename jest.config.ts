fe26587513305f19c7a598f46f43248fcbb6cbe0
Moisés Junior <adsmoisa@gmail.com>
2026-06-30 00:24:08 -0300

    chore: scaffold Next 16 + React 19 + Tailwind 4 + TS

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest'
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^server-only$': '<rootDir>/__mocks__/server-only.js',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {}],
+    '^.+\\.js$': ['ts-jest', { tsconfig: { allowJs: true } }],
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(nanoid)/)'],
+}
+export default config
